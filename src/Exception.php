3a34411e28ccb11b45d69d005fc4ec19655ba290
Alexey Borzov <avb@php.net>
2023-08-14 00:25:37 +0300

    Add exception classes

---
+++ src/Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of sad_spirit/pg_gateway package
+ *
+ * (c) Alexey Borzov <avb@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace sad_spirit\pg_gateway;
+
+/**
+ * Interface that should be implemented by all package exceptions
+ */
+interface Exception extends \Throwable
+{
+}
---
+++ src/exceptions/InvalidArgumentException.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of sad_spirit/pg_gateway package
+ *
+ * (c) Alexey Borzov <avb@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace sad_spirit\pg_gateway\exceptions;
+
+use sad_spirit\pg_gateway\Exception;
+
+/**
+ * Namespaced version of SPL's InvalidArgumentException
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements Exception
+{
+}
