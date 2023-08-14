9237e0030a120ae0da87207a1bd0dcfce0f61376
Alexey Borzov <avb@php.net>
2023-08-14 11:35:54 +0300

    Github CI setup

---
+++ ci/github-bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of sad_spirit/pg_gateway package
+ *
+ * (c) Alexey Borzov <avb@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+
+require_once __DIR__ . '/github-config.php';
---
+++ ci/github-config.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of sad_spirit/pg_gateway package
+ *
+ * (c) Alexey Borzov <avb@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace sad_spirit\pg_gateway\tests;
+
+const CONNECTION_STRING = 'host=127.0.0.1 user=postgres password=postgres dbname=pg_gateway_test';
